3bdceba77ec742f3c6a34405c9fbe406bca76b64
jayhi2024 <ht.0810.01@gmail.com>
2025-03-28 22:57:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(express.json(public));
+app.use(express.json());             
+app.use(express.static("public")); 
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
